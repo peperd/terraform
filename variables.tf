22c7f557a614bb387123d19cff837e16ec9dc8d1
peperd <d.zuzuk@gmail.com>
2024-05-11 20:28:56 +0000

    changes

--- variables.tf
+++ variables.tf
@@ -1,10 +1,28 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project name"
+}
 variable "GOOGLE_REGION" {
   type        = string
   description = "GCP region name"
 }
-variable "GOOGLE_PROJECT" {
-  type        = string
-  description = "GCP project name"
+variable "GITHUB_OWNER" {
+  type = string
+  description = "Github owner repository to use"
+}
+variable "GITHUB_TOKEN" {
+  type = string
+  description = "Github personal access token"
+}
+variable "FLUX_GITHUB_REPO" {
+  type = string
+  default = "flux-gitops"
+  description = "Flux Gitops repository"
+}
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type = string
+  default = "clusters"
+  description = "Flux manifests subdirectory"
 }
 variable "GKE_NUM_NODES" {
   type        = number
@@ -13,4 +31,4 @@ variable "GKE_NUM_NODES" {
 provider "google" {
   project = var.GOOGLE_PROJECT
   region  = var.GOOGLE_REGION
-}
+} 
